e8e8800a34bf90370c3cbb266bde06865c1822e5
MARCO ROMAN s343791 <s343781@studenti.polito.it>
2025-09-21 15:55:49 +0200

    rete git normalizzata

--- Cnn.py
+++ Cnn.py
@@ -19,30 +19,36 @@ class AgeCNN(nn.Module):
         super(AgeCNN, self).__init__()
         # Input: grayscale (1 channel), 200x200
         self.conv1 = nn.Conv2d(1, 32, kernel_size=3)
+        self.bn1 = nn.BatchNorm2d(32)
         self.pool = nn.AvgPool2d(2, 2)   # same as AveragePooling2D
 
         self.conv2 = nn.Conv2d(32, 64, kernel_size=3)
+        self.bn2 = nn.BatchNorm2d(64)
         self.conv3 = nn.Conv2d(64, 128, kernel_size=3)
+        self.bn3 = nn.BatchNorm2d(128)
         self.conv4 = nn.Conv2d(128, 256, kernel_size=3)
+        self.bn4 = nn.BatchNorm2d(256)
 
         # Global Average Pooling
         self.global_pool = nn.AdaptiveAvgPool2d((1, 1))  # like GlobalAveragePooling2D
 
         # Dense layers
         self.fc1 = nn.Linear(256, 132)
-        self.fc2 = nn.Linear(132, 9)  
+        self.dropout1 = nn.Dropout(0.5)
+        self.fc2 = nn.Linear(132, 9)
+        self.dropout2 = nn.Dropout(0.5)
 
     def forward(self, x):
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
-        x = self.pool(F.relu(self.conv3(x)))
-        x = self.pool(F.relu(self.conv4(x)))
+        x = self.pool(F.relu(self.bn1(self.conv1(x))))
+        x = self.pool(F.relu(self.bn2(self.conv2(x))))
+        x = self.pool(F.relu(self.bn3(self.conv3(x))))
+        x = self.pool(F.relu(self.bn4(self.conv4(x))))
 
-        x = self.global_pool(x)        
-        x = torch.flatten(x, 1)       
+        x = self.global_pool(x)
+        x = torch.flatten(x, 1)
 
-        x = F.relu(self.fc1(x))
-        x = self.fc2(x)                
+        x = self.dropout1(F.relu(self.fc1(x)))
+        x = self.dropout2(self.fc2(x))
         return x
 
 
